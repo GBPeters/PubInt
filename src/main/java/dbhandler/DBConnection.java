3d1dec9b7c69bf8bf2cdfd71b789d113dd26905f
GBPeters <gijstebrij@gmail.com>
2016-10-03 12:13:41 +0200

    Documentation added to existing classes

--- src/main/java/dbhandler/DBConnection.java
+++ src/main/java/dbhandler/DBConnection.java
@@ -1,28 +1,41 @@
 package dbhandler;
 
-import org.postgresql.*;
-import org.postgresql.Driver;
-
 import java.sql.*;
 
 /**
+ * Basic PostgreSQL database handler for connections to localhost:5432/pubint
+ *
  * Created by gijspeters on 02-10-16.
  */
 public class DBConnection {
 
     public static final String HOST = "localhost";
     public static final String PORT = "5432";
-    public static final String NAME = "tweets";
+    public static final String NAME = "pubint";
     public static final String USER = "postgres";
     public static final String PASS = "postgres";
 
     private Connection connection;
     private boolean autoCommit;
 
+    /**
+     * Default constructor for a database connection
+     * Autocommit is false by default
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     public DBConnection() throws SQLException, ClassNotFoundException {
         setupConnection(false);
     }
 
+    /**
+     * Constructor that allows setting of autocommit
+     *
+     * @param autocommit
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public DBConnection(boolean autocommit) throws ClassNotFoundException, SQLException {
         setupConnection(autocommit);
     }
@@ -39,14 +52,34 @@ public class DBConnection {
         connection = c;
     }
 
+    /**
+     * Method for executing queries with responses
+     *
+     * @param query A valid SQL query
+     * @return
+     * @throws SQLException
+     */
     public ResultSet select(String query) throws SQLException {
         return connection.createStatement().executeQuery(query);
     }
 
+    /**
+     * Method for executing other statements without responses.
+     *
+     * @param query A valid SQL statement
+     * @throws SQLException
+     */
     public void execute(String query) throws SQLException {
         execute(query, doesAutoCommit());
     }
 
+    /**
+     * Method for executing other statements without responses.
+     *
+     * @param query  A valid SQL statement
+     * @param commit Set to true for instant commit
+     * @throws SQLException
+     */
     public void execute(String query, boolean commit) throws SQLException {
         Statement stmt = connection.createStatement();
         stmt.execute(query);
@@ -55,18 +88,38 @@ public class DBConnection {
         }
     }
 
+    /**
+     * Commit database changes
+     *
+     * @throws SQLException
+     */
     public void commit() throws SQLException {
         connection.commit();
     }
 
+    /**
+     * Get autocommit setting
+     *
+     * @return boolean autocommit
+     */
     public boolean doesAutoCommit() {
         return autoCommit;
     }
 
+    /**
+     * Set autocommit
+     *
+     * @param autoCommit true to enable autocommit
+     */
     public void setAutoCommit(boolean autoCommit) {
         this.autoCommit = autoCommit;
     }
 
+    /**
+     * Closes the database connection. DBConnection object becomes useless after close.
+     *
+     * @throws SQLException
+     */
     public void close() throws SQLException {
         connection.close();
     }
--- src/main/java/otpentry/OTPHandler.java
+++ src/main/java/otpentry/OTPHandler.java
@@ -10,22 +10,45 @@ import java.util.Date;
 
 /**
  * Created by gijspeters on 02-10-16.
+ *
+ * This class creates an entry point to OTP and handles all requests to OTP.
+ *
  */
 public class OTPHandler {
 
     private OtpsEntryPoint otp;
     public enum ROUTE_MODES {FROM_ORIGIN, TO_DESTINATION}
 
+    /**
+     * Constructor with default graph directory (/var/otp/graphs/)
+     *
+     * @throws Exception
+     */
     public OTPHandler() throws Exception {
         String[] args = {"--autoScan"};
         this.otp = OtpsEntryPoint.fromArgs(args);
     }
 
+    /**
+     * Constructor for custom graph directory
+     * @param graphDir The graph directory
+     * @throws Exception
+     */
     public OTPHandler(String graphDir) throws Exception {
         String[] args = {"--graphs", graphDir, "--autoScan"};
         this.otp = OtpsEntryPoint.fromArgs(args);
     }
 
+    /**
+     * Creates a shortest path tree from a given origin or to a given destination
+     * for the given amount of time
+     *
+     * @param coord The origin or destination coordinate
+     * @param date  The date and time of departure or arrival
+     * @param maxTime The maximum amount of time in seconds to calculate the shortest path tree for
+     * @param mode Whether the shortest path tree is calculated from an origin, or to a destination
+     * @return The calculated shortest path tree
+     */
     public ShortestPathTree getShortestPathTree(Coordinate coord, Date date, long maxTime, ROUTE_MODES mode) {
         OtpsRouter router = otp.getRouter();
         OtpsRoutingRequest req = otp.createRequest();
